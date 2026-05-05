4e6f2a9b37203b514402d6f97f7f0b75f25764d7
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-05-05 22:58:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'tw-radio-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/@phosphor-icons/web'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // We catch errors on external CDNs so they don't break installation
+        return Promise.all(urlsToCache.map(url => {
+            return cache.add(url).catch(err => console.log('Fetch failed for', url, err));
+        }));
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  // Ignore audio streams caching to prevent issues
+  if (event.request.url.includes('stream') || event.request.url.includes('api')) return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
